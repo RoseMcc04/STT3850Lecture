969d5914a339b90583f3918a09d4b7c3b5e25437
RoseMcc04 <mccormacka@appstate.edu>
2024-08-19 01:21:38 -0400

    first saved version for branch

---
+++ IntroNotes/Demo.R
@@ -0,0 +1,22 @@
+'''
+@author Rose McCormack
+@version 19 August 2024
+'''
+
+# This is a comment. 
+
+# R as a calculator
+
+x <- 3 + 3 # addition
+
+y <- 10 - 4 # subtraction
+
+z <- 6 * 7 # multiplication
+
+aA <- 120 / 10 # division
+
+# More complex expressions
+
+r <- 1
+h <- 10
+volume <- pi * (r ^ 2) * h
